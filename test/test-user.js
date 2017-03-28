4a3c1a1751a3a67b2877ef75eac8d7f086f417a7
Tomas Ruiz <tomastrg22@gmail.com>
2017-03-28 15:38:44 +0200

    error

--- test/test-user.js
+++ test/test-user.js
@@ -5,21 +5,34 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../app');
 const should = chai.should();
-//const url = 'http://localhost:3000';
-const url = 'https://twitter-info-scrapping.herokuapp.com';
+const url = 'http://localhost:3000';
+const getUserInfo = '../controllers/scrappedInfo.controller';
+//const url = 'https://twitter-info-scrapping.herokuapp.com';
+
 
 chai.use(chaiHttp);
 
+
 /*Test for /api/user/:username endpoint*/
 describe('Get twitter info', () => {
-  it('it should GET an object', (done => {
+  // it('it should GET an object', (done => {
+  //   chai.request(url)
+  //     .get('/api/user/influencity')
+  //     .end((err, res) => {
+  //       res.should.be.a('object');
+  //     });
+  // }));
+
+  it('it should GET an object', async(done => {
     chai.request(url)
-      .get('/api/user/influencity')
+      .get('/api/user/influencity', (req, res) => {
+       await  res.should.be.a('object');
+      })
       .end((err, res) => {
         res.should.be.a('object');
       });
-    done();
   }));
+
 
   it('it should GET an object with id property', (done => {
     chai.request(url)
