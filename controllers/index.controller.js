8c4a775055ea50b219d08c559e9effca059eb308
Tomas Ruiz <tomastrg22@gmail.com>
2017-03-28 12:13:00 +0200

    index added

---
+++ controllers/index.controller.js
@@ -0,0 +1,4 @@
+/*jshint esversion:6*/
+exports.getIndex = (req, res, next) => {
+  res.render('index');
+};
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ const scrappingcontroller = require('../controllers/scrappedInfo.controller');
 const logController = require('../controllers/scrappingLog.controller');
 const errorController = require('../controllers/error.controller');
 const fakeXRay = require('../controllers/fake-x-ray.controller');
-
+const index = require('../controllers/index.controller');
+router.get('/', index.getIndex);
 router.get('/fake-x-ray/get-string', fakeXRay.getString);
 router.get('/fake-x-ray/get-object', fakeXRay.getObject);
 router.get('/fake-x-ray/get-array', fakeXRay.getArray);
