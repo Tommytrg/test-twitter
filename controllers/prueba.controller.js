3187b49f60c32b688fb0a0850d2bf814f652380a
Tomas Ruiz <tomastrg22@gmail.com>
2017-03-29 12:49:59 +0200

    promise try

--- controllers/prueba.controller.js
+++ controllers/prueba.controller.js
@@ -6,16 +6,25 @@ const x = Xray();
 const url = 'https://twitter.com/';
 
 exports.test = (req, res, next) => {
-  const address =  url + req.params.username;
-  console.log(address);
-  x(address, '.u-linkComplex-target')((err, name) => {
+  const scrappingName = new Promise((resolve, reject) => {
+    getScreenName(req, res, resolve);
+  });
+
+  scrappingName.then((name) => {
+    res.status(200).json(name);
+  });
+};
+
+const getScreenName = (req, res, resolve) => {
+  x(url + req.params.username, '.u-linkComplex-target')((err, name) => {
     if (err) {
       let msg = 'error getting screen_name';
       return handleError(res, msg, err);
     }
-    return res.status(200).json(name);
+    resolve(name);
   });
 };
+
 
 const handleError = (res, msg, err) => {
   return res.status(400).json({
